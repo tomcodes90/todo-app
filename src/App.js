b916ada89065416da5480f3b2cdd24deaf2bfb2d
Thomas Miccichè <97365235+tomcodes90@users.noreply.github.com>
2022-06-12 21:39:07 +0200

    First approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Button, Alert, Breadcrumb, Card } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-dark-mode">
+        <Card className="mb-3" style={{ color: "black", width: "18rem" }}>
+          <Card.Img src="./images/bg-mobile-dark.jpg" />
+          <Card.Body>
+            <Card.Title>Card Example</Card.Title>
+            <Card.Text>This is an example of React Bootstrap cards</Card.Text>
+            <Button variant="primary">Read More</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test</Breadcrumb.Item>
+          <Breadcrumb.Item>Test</Breadcrumb.Item>
+          <Breadcrumb.Item>Test</Breadcrumb.Item>
+        </Breadcrumb>
+        <Button>Test Button</Button>
+        <Alert variant="success"> That's a Button</Alert>
+      </div>
     </div>
   );
 }
